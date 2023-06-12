eeb5cf500c550b3c092f635b7eacea2235a3b198
noelfrancis12 <xxxnoeleonxxx@gmail.com>
2023-06-12 19:26:44 +0530

    second commit

--- restapp/urls.py
+++ restapp/urls.py
@@ -3,4 +3,7 @@ from django.urls import path,include
 from.import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('menu',views.menu,name='menu'),
+    path('gallery',views.gallery,name='gallery'),
+    path('cart',views.cart,name='cart'),
 ]
--- restapp/views.py
+++ restapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+def menu(request):
+    return render(request,'menu.html')
+def gallery(request):
+    return render(request,'gallery.html')
+def cart(request):
+    return render(request,'cart.html')
